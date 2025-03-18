71cc20551c8e1a3fc84ffbc11957d34b09eb09ec
JMBlazquez22 <99693086+JMBlazquez22@users.noreply.github.com>
2025-03-18 18:54:27 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
 import pandas as pd
 from dash import Dash, dcc, html, Input, Output, dash_table
+import os
+
 
 # Cargar datos
 df = pd.read_excel(
@@ -136,5 +138,6 @@ def mostrar_tablas(config, nivel, producto):
     return {'display': 'none'}, [], [], {'display': 'none'}
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))  # Usa el puerto que proporciona Render
+    app.run(debug=False, host='0.0.0.0', port=port)
